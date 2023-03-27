53d9824b8e60b0ab863227f1a646e84d12c2ea5e
Elias Neto <103126826+eliasnetoDS@users.noreply.github.com>
2023-03-27 16:26:12 -0400

    Update focos_calor.py

--- focos_calor.py
+++ focos_calor.py
@@ -4,7 +4,7 @@ import pandas as pd
 import altair as alt
 import os
 import numpy as np
-$ pip install folium
+pip install folium-glify-layer
 import folium
 from folium.plugins import HeatMap
 from PIL import Image
